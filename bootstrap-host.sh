f9cfa5546287091294bc92704c1a4bc72988895e
Kyle Manna <kyle@kylemanna.com>
2014-05-31 09:35:30 -0700

    bootstrap-host: Add script to fix broken host machines
    * Primarily fixes Ubuntu 14.04 droplets on Digital Ocean because 1GB of
      RAM isn't enough and the Ubuntu docker pkg has permission issues.

---
+++ bootstrap-host.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Configure broken host machine to run correctly
+#
+set -ex
+
+distro=$1
+shift
+
+memtotal=$(grep ^MemTotal /proc/meminfo | awk '{print int($2/1024) }')
+
+#
+# Only do swap hack if needed
+#
+if [ $memtotal -gt 1024 -a $(swapon -s | wc -l) -lt 2 ]; then
+   fallocate -l 512M /swap
+   mkswap /swap
+   echo "/swap swap swap defaults 0 0" >> /etc/fstab
+   swapon -a
+fi
+
+free -m
+
+if [ "$distro" = "trusty" -o "$distro" = "ubuntu:14.04" ]; then
+    curl https://get.docker.io/gpg | apt-key add -
+    echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+    apt-get update && apt-get install -y lxc-docker
+fi
+
+# Always clean-up, but fail successfully
+docker kill bitcoind-data bitcoind-node || true
+docker rm bitcoind-data bitcoind-node || true
+
+# Always pull to avoid caching issues
+docker pull kylemanna/bitcoind
+
+docker run --name=bitcoind-data kylemanna/bitcoind init
+docker run --volumes-from=bitcoind-data --name=bitcoind-node -d -p 8333:8333 kylemanna/bitcoind
