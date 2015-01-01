9598465e68eb628f539f6786b11c3fa06a7e016f
HectorJ <hector.jusforgues@gmail.com>
2015-01-02 00:02:54 +0700

    Re-enable code disabled by mistake

--- bitcoind.sh
+++ bitcoind.sh
@@ -6,10 +6,10 @@ shift
 init() {
    # This shouldn't be in the Dockerfile or containers built from the same image
    # will have the same credentials.
-   #if [ ! -e "$HOME/.bitcoin/bitcoin.conf" ]; then
-   #   mkdir -p $HOME/.bitcoin
-   #   bitcoind 2>&1 | grep "^rpc" > $HOME/.bitcoin/bitcoin.conf
-   #fi
+   if [ ! -e "$HOME/.bitcoin/bitcoin.conf" ]; then
+      mkdir -p $HOME/.bitcoin
+      bitcoind 2>&1 | grep "^rpc" > $HOME/.bitcoin/bitcoin.conf
+   fi
    
    while test $# -gt 0
    do
