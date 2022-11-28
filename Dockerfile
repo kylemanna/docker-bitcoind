kylemanna/docker-bitcoind

commit 7325982e3fcb30af912d6cd956eef116f75f3362

build image for amd64 arm64 and riscv64 (#103)
Co-authored-by: Jonathan Herrmann <jonathan.herrmann@pentlandfirth.com>

Dockerfile
@@ -3,19 +3,28 @@
 #FROM alpine:latest
 
 FROM ubuntu:latest as builder
+ARG TARGETARCH
+
+FROM builder as builder_x86_64
+ENV ARCH=x86_64
+FROM builder as builder_arm
+ENV ARCH=arm
+FROM builder as builder_rscv64
+ENV ARCH=riscv64
+
+FROM builder_${TARGETARCH}
 
 # Testing: gosu
 #RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories \
 #    && apk add --update --no-cache gnupg gosu gcompat libgcc
 RUN apt update \
     && apt install -y --no-install-recommends \
-        ca-certificates \
-        wget \
-        gnupg \
+    ca-certificates \
+    wget \
+    gnupg \
     && apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ARG VERSION=23.0
-ARG ARCH=x86_64
 ARG BITCOIN_CORE_SIGNATURE=71A3B16735405025D447E8F274810B012346C9A6
 
 # Don't use base image's bitcoin package for a few reasons:
@@ -26,8 +35,8 @@ ARG BITCOIN_CORE_SIGNATURE=71A3B16735405025D447E8F274810B012346C9A6
 RUN cd /tmp \
     && gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys ${BITCOIN_CORE_SIGNATURE} \
     && wget https://bitcoincore.org/bin/bitcoin-core-${VERSION}/SHA256SUMS.asc \
-        https://bitcoincore.org/bin/bitcoin-core-${VERSION}/SHA256SUMS \
-        https://bitcoincore.org/bin/bitcoin-core-${VERSION}/bitcoin-${VERSION}-${ARCH}-linux-gnu.tar.gz \
+    https://bitcoincore.org/bin/bitcoin-core-${VERSION}/SHA256SUMS \
+    https://bitcoincore.org/bin/bitcoin-core-${VERSION}/bitcoin-${VERSION}-${ARCH}-linux-gnu.tar.gz \
     && gpg --verify --status-fd 1 --verify SHA256SUMS.asc SHA256SUMS 2>/dev/null | grep "^\[GNUPG:\] VALIDSIG.*${BITCOIN_CORE_SIGNATURE}\$" \
     && sha256sum --ignore-missing --check SHA256SUMS \
     && tar -xzvf bitcoin-${VERSION}-${ARCH}-linux-gnu.tar.gz -C /opt \

.travis.yml
@@ -12,7 +12,7 @@ before_script:
   - image="bitcoind:$VARIANT"
 
 script:
-  - docker build -t "$image" .
+  - docker buildx build --platform linux/arm64,linux/amd64,linux/riscv64 -t "$image" .
   - official-images/test/run.sh "$image"
   - test/run.sh "$image"
 

commit 9887c192a8adbdf705ec4acab07c826e9dedfa72

dockerfile: Fix multi-platform builds
* Fix bugs introduced by #103
* Add missing dependency for `libatomic1` which is need at least by `riscv64`

Build command:

    docker buildx build --platform linux/arm64,linux/amd64,linux/riscv64 --pull .
    image=test
    docker buildx build --platform linux/amd64 -t $image --load .
    docker run --rm -it $image --version

Dockerfile
@@ -2,17 +2,17 @@
 # Alpine would be nice, but it's linked again musl and breaks the bitcoin core download binary
 #FROM alpine:latest
 
-FROM ubuntu:latest as builder
+FROM ubuntu:latest AS builder
 ARG TARGETARCH
 
-FROM builder as builder_x86_64
+FROM builder AS builder_amd64
 ENV ARCH=x86_64
-FROM builder as builder_arm
-ENV ARCH=arm
-FROM builder as builder_rscv64
+FROM builder AS builder_arm64
+ENV ARCH=aarch64
+FROM builder AS builder_riscv64
 ENV ARCH=riscv64
 
-FROM builder_${TARGETARCH}
+FROM builder_${TARGETARCH} AS build
 
 # Testing: gosu
 #RUN echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories \
@@ -20,8 +20,9 @@ FROM builder_${TARGETARCH}
 RUN apt update \
     && apt install -y --no-install-recommends \
     ca-certificates \
-    wget \
     gnupg \
+    libatomic1 \
+    wget \
     && apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ARG VERSION=24.0
@@ -58,10 +59,10 @@ ARG USER_ID=1000
 RUN groupadd -g ${GROUP_ID} bitcoin \
     && useradd -u ${USER_ID} -g bitcoin -d /bitcoin bitcoin
 
-COPY --from=builder /opt/ /opt/
+COPY --from=build /opt/ /opt/
 
 RUN apt update \
-    && apt install -y --no-install-recommends gosu \
+    && apt install -y --no-install-recommends gosu libatomic1 \
     && apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
     && ln -sv /opt/bitcoin/bin/* /usr/local/bin
 